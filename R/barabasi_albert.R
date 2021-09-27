96412953edb60c528bcab94528f573a2fb3b5741
Martí Renedo <marti.renedo@gmail.com>
2021-09-27 05:16:18 +0200

    Updated Barabasi-Albert block model with support for both the Hajek and the block-first method.

--- R/barabasi_albert.R
+++ R/barabasi_albert.R
@@ -1,5 +1,3 @@
-
-
 #' Generates a Barabási-Albert graph with community structure
 #' 
 #' @param t_max final graph order
@@ -9,9 +7,8 @@
 #' @param m number of edges added at each step.
 #' @param G0 initial graph
 #' @param G0_labels labels of the initial graph. If NULL, they will al be set to 1.
-#' @param poisson If TRUE, instead of adding m edges incident to each new vertex, 
-#' the number of new edges is sampled from a Poisson distribution with parameter m.
 #' @param sample_with_replacement If TRUE, allows parallel edges.
+#' @param type Either "Hajek" or "block_first".
 #' 
 #' @return The resulting graph, as an igraph object. The vertices have a
 #' "label" attribute.
@@ -20,7 +17,7 @@
 #' @export
 barabasi_albert_blocks <- function(m, p, B, G0=NULL, G0_labels=NULL, t_max,
                                    sample_with_replacement=FALSE,
-                                   poisson=FALSE){
+                                   type="Hajek"){
 
     if (is.null(G0)){
         G0 <- generate_G0(n=5*m, p=p, m=m, B=B)
@@ -29,10 +26,6 @@ barabasi_albert_blocks <- function(m, p, B, G0=NULL, G0_labels=NULL, t_max,
     G <- G0
     t0 <- gorder(G) + 1
     
-    if (poisson){ 
-        lambda <- m
-    }
-    
     new_labels <- sample(1:length(p), t_max-t0+1, replace=TRUE, prob=p)
     if (is.null(G0_labels)){
         G0_labels <- rep(1, gorder(G0))
@@ -40,37 +33,79 @@ barabasi_albert_blocks <- function(m, p, B, G0=NULL, G0_labels=NULL, t_max,
     labels <- c(G0_labels, new_labels)
     
     degrees <- rep(0, t_max)
-    degrees[0:(t0-1)] <- degree(G0)
+    degrees[1:(t0-1)] <- degree(G0)
     
-    new_edgelist <- matrix(nrow=(t_max-t0+1)*m , ncol=2)
-    i <- 1
-    for (t in t0:t_max){
-        weights <- degrees[1:(t-1)] * B[labels[1:(t-1)], labels[t]]
-        if (poisson){
-            m <- rpois(1, lambda)
-        }
-        new_half_edges <- sample(t-1, size=m, replace=sample_with_replacement, prob=weights)
-        
-        degrees[t] <- m
-        for (j in 1:m){
-            v <- new_half_edges[j]
-            degrees[v] <- degrees[v] + 1
-            new_edgelist[i,] <- c(t, v)
-            i <- i+1
-        }
+    l <- as.list(t0:t_max)
+    
+    if (type == "block_first"){
+        fa <-  first_appearance(labels)
     }
     
+    for (t in t0:t_max){
+        if (type == "Hajek"){
+            weights <- degrees[1:(t-1)] * B[labels[1:(t-1)], labels[t]]
+            new_half_edges <- sample(t-1, size=m, replace=sample_with_replacement, prob=weights)
+            el <- matrix(nrow=m, ncol=2) #edgelist of new edges
+            degrees[t] <- m
+            for (j in 1:m){
+                v <- new_half_edges[j]
+                degrees[v] <- degrees[v] + 1
+                el[j,] <- c(t, v)
+            }
+        }
+        
+        if (type == "block_first"){
+            populated_communities <- which(fa < t)
+            new_half_edge_blocks <- sample(populated_communities, size=m, 
+                                           replace=TRUE,
+                                           prob = B[labels[t], populated_communities])
+            el <- matrix(nrow=m, ncol=2) #edgelist of new edges
+            k <- 1 
+            for (j in unique(new_half_edge_blocks)){
+                candidates = which(labels[1:t-1]==new_half_edge_blocks[j])
+                n_half_edges = sum(new_half_edge_blocks==j)
+                print(candidates)
+                print(n_half_edges)
+                connected_vertices <- sample(candidates, size=n_half_edges, prob=degrees[candidates],
+                            replace=sample_with_replacement)
+                for (v in connected_vertices){
+                    degrees[v] <- degrees[v] + 1
+                    el[k,] <- c(t, v)
+                    k <- k+1
+                }
+            }
+        }
+        l[[t-t0+1]] <- el
+    }
+    
+    complete_edgelist <- do.call(rbind, l)
+
     G <- add.vertices(G0, nv=t_max-t0+1)
-    G <- add.edges(G, as.vector(t(new_edgelist)))
+    G <- add.edges(G, as.vector(t(complete_edgelist)))
     V(G)$label <- labels
     return(G)
 }
 
+first_appearance <- function(labels){
+    # "labels" is a vector of ints with values 1, ..., n
+    n <- max(labels)
+    first_appearance <- rep(Inf, n)
+    for (i in 1:length(labels)){
+        l <- labels[i]
+        if (is.infinite(first_appearance[l])){
+            first_appearance[l] <- i
+        }
+    }
+    return(first_appearance)
+}
+
+
 generate_G0 <- function(n, p, m, B){
     #TO DO: 
     # -verify that there are enough vertices so that we don't need to add more edges than possible
+    # -add isolated vertices to the graph (if creating from edgelist this doesn't happen)
 
-    
+    n <- round(n)
     
     labels <- sample(length(p), n, replace=TRUE, prob=p)
     potential_edges <- matrix(0, nrow=n*(n-1)/2, ncol=3) #first and second columns are adjacent vertices, third is the weight of the edge probability
@@ -81,7 +116,7 @@ generate_G0 <- function(n, p, m, B){
             k <- k+1
         }
     }
-    selected_edges <- sample(nrow(potential_edges), size=m*n, prob=potential_edges[,3])
+    selected_edges <- sample(nrow(potential_edges), size=round(m*n), prob=potential_edges[,3])
     selected_edgelist <- potential_edges[selected_edges,1:2]
     G0 <- graph_from_edgelist(selected_edgelist, directed=FALSE)
     V(G0)$label <- labels
