3708b73167e35203ae4eb2c50623fff89d973d4d
martirm <marti.renedo@gmail.com>
2021-11-16 11:57:31 +0100

    removed old function that is no longer used

--- R/scoring_functions_Rcpp.R
+++ R/scoring_functions_Rcpp.R
@@ -35,16 +35,17 @@ out_degree_fractions <- function(g, com, edgelist){
 
 #' Scoring Functions of a Graph Partition
 #'
-#' Computes the scoring functions of a graph and its clusters
-#' @param g Graph to be analyzed (as an igraph object)
-#' @param com Community membership vector. Each element corresponds to a vertex
+#' Computes the scoring functions of a graph and its clusters.
+#' @param g Graph to be analyzed (as an igraph object). It can be a weighted graph, 
+#' in which case the weights of the edges
+#' @param com Community membership integer vector. Each element corresponds to a vertex
 #' of the graph, and contains the index of the community it belongs to.
 #' @param type can be "local" for a cluster by cluster analysis, or "global" for
 #' a global analysis of the whole graph partition.
 #' @param weighted Is the graph weighted? If it is, doesn't compute TPR score.
-#' @param w_max Upper bound for edge weights. Should be generally left as default (NULL).
+#' @param w_max Numeric. Upper bound for edge weights. Should be generally left as default (NULL).
 #' Only affects the computation of the clustering coefficient.
-#' @param no_clustering_coef  If TRUE, skips the computation of the clustering
+#' @param no_clustering_coef  Logical. If TRUE, skips the computation of the clustering
 #' coefficient (which can be slow on large graphs).
 #'
 #' @return If \code{type=="local"}, returns a dataframe with a row for each
@@ -52,6 +53,7 @@ out_degree_fractions <- function(g, com, edgelist){
 #' single row with the weighted average scores.
 #'
 #' @examples
+#' data(karate, package="igraphdata")
 #' scoring_functions(karate, membership(cluster_louvain(karate)))
 #' @export
 scoring_functions <- function(g, com, no_clustering_coef=TRUE,
@@ -142,58 +144,3 @@ FOMD <- function(g, com, edgelist){
 
 
 
-
-scoring_functions_df <- function(G,com, ignore_NA=TRUE, no_clustering_coef=TRUE, type="local", weighted=FALSE){
-    # type: can be "local" or "global", depending on whether we want a cluster-by cluster or a
-    # global analysis
-
-    if (!"weight" %in% names(edge.attributes(G)))
-        G <- set_edge_attr(G, "weight", value=1)
-
-    n_com <- max(com)
-    n <- length(V(G))
-    function_names <- c("size", "internal density","edges inside","av degree","FOMD","expansion",
-                        "cut ratio","conductance", "norm cut", "max ODF","average ODF","flake ODF",
-                        "TPR", "clustering coef","modularity")
-    D <- data.frame(matrix(nrow=n_com,ncol=length(function_names)))
-    colnames(D) <- function_names
-    rownames(D) <- c(1:n_com)
-
-    for (i in 1:n_com){
-        s <- which(com==i) #s contains the indices of vertices belonging to cluster i
-        size <- length(s)
-        Gs <- subgraph(G,s)
-        if (weighted){
-            if (no_clustering_coef)
-                cc <- NaN
-            else
-                cc <- clustering_coef_subgraph(G, s, w_max=w_max)
-        }
-        else{
-            cc <- transitivity(Gs)
-        }
-        D[i,]<- c(size, internal_density_s(G,s),m_subgraph_w(G,s),average_degree(G,s),frac_over_median(G,s),expansion(G,s),
-                  cut_ratio(G,s),conductance(G,s),normalized_cut(G,s),max_odf(G,s),av_odf(G,s),flake_odf(G,s),
-                  triangle_participation_ratio(Gs), cc, NaN)
-    }
-    if (type=="local"){
-        D[,"modularity"] <- modularity(G, com)
-        D$coverage <- coverage(G, com)
-        return(D)
-    }
-
-
-    # type=="global" from here
-    D_glob <- apply(D,2,weighted.mean, w=D$size)
-    D_glob["size"] <- NaN
-    D_glob["graph_size"] <- n
-    D_glob["n_clusters"] <- n_com
-    D_glob["mean_cluster_size"] <- mean(D$size)
-
-    D_glob["modularity"] <- modularity(G, com)
-    D_glob["coverage"] <- coverage(G, com)
-    D_glob["density ratio"] <- density_ratio(G, com)
-    return(t(as.matrix(D_glob)))
-
-
-}
