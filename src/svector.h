6a7e16c532dd225e801685103fe31d2707d934a9
martirm <marti.renedo@gmail.com>
2021-11-23 13:19:49 +0100

    removed old unused code from svector.h
    All related to the random sampling of elements.

--- src/svector.h
+++ src/svector.h
@@ -3,7 +3,6 @@
 
 #include <map>
 #include <vector>
-#include <random>
 #include <Rcpp.h>
 
 
@@ -16,16 +15,11 @@ class SVector{
         std::vector<T> v;
         std::map<T, int> m;
         int l;
-        std::mt19937 generator;
-        std::uniform_int_distribution<int> distribution;
     public:
         SVector(){
             v = std::vector<T>();
             l = 0;
             m = std::map<T, int>();
-            std::random_device device;
-            generator = std::mt19937(device());
-            distribution = std::uniform_int_distribution<int> (0, l-1);
         }
         SVector(std::vector<T> v0){
             v = v0;
@@ -33,12 +27,6 @@ class SVector{
             for (int i=0; i<l; ++i){
                 m[v[i]] = i;
             }
-            std::random_device device;
-            generator = std::mt19937(device());
-            distribution = std::uniform_int_distribution<int> (0, l-1);
-        }
-        void update_distribution(){
-            distribution = std::uniform_int_distribution<int> (0, l-1);
         }
         void insert(T a){
             //does nothing if a is already there
@@ -47,7 +35,6 @@ class SVector{
                 v.push_back(a);
                 m[a] = v.size()-1;
                 ++l;
-                update_distribution();
             }
         }
         void remove(T a){
@@ -62,7 +49,6 @@ class SVector{
                 }
                 v.pop_back();
                 --l;
-                update_distribution();
             }
         }
         
@@ -72,21 +58,10 @@ class SVector{
             return not (it == m.end());
         }
         
-        T rand_el_old(){
-            int i = distribution(generator);
-            std::cout << "random index: " << i << ", l=" << l << std::endl;
-            return v[i];
-            //return (v[distribution(generator)]);
-        }
         T rand_el(){
             auto i = Rcpp::sample(l, 1);
             return v[i[0]-1];
         }
-        // vector<T> sample(int n){
-        //     std::vector<T> s(n);
-        //     std::sample(v.begin(), v.end(), s.begin(), n);
-        //     return (s);
-        // }
 };
 
 
