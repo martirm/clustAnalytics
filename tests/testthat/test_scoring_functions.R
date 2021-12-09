06cf58141be92d7b199012a9a42d90d0f52f4bf0
martirm <marti.renedo@gmail.com>
2021-12-09 01:47:24 +0100

    use attributes in karate graph as GT clustering
    Instead of manually including a membership vector separately.

--- tests/testthat/test_scoring_functions.R
+++ tests/testthat/test_scoring_functions.R
@@ -1,12 +1,11 @@
 
 
 library(igraphdata)
-data("karate")
-karate_gt_clustering <- c(1,1,1,1,1,1,1,1,1,2,1,1,1,1,2,2,1,1,
-                          2,1,2,1,2,2,2,2,2,2,2,2,2,2,2,2)
+data(karate, package="igraphdata")
+
 
 test_that("test scoring functions", {
-    expect_equal_to_reference(scoring_functions(karate, karate_gt_clustering, 
+    expect_equal_to_reference(scoring_functions(karate, V(karate)$Faction, 
                                                 no_clustering_coef = FALSE),
                               file="test_cache_scoring_functions_1")
     
