4cb850fb51a8d630032e9206f7c216c8e562775e
Martí Renedo <marti.renedo@gmail.com>
2022-05-24 13:11:15 +0200

    Update scoring_functions_Rcpp.R

--- R/scoring_functions_Rcpp.R
+++ R/scoring_functions_Rcpp.R
@@ -63,7 +63,7 @@ scoring_functions <- function(g, com, no_clustering_coef=TRUE,
                               type="local", weighted=TRUE, w_max=NULL){
 
     if (!"weight" %in% names(edge.attributes(g)) | weighted==FALSE){
-        G <- set_edge_attr(g, "weight", value=1)
+        g <- set_edge_attr(g, "weight", value=1)
         w_max <- 1
     }
 
